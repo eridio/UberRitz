8049bf763aeb54ea15615ade56c37b48b7c5037f
flo <eridio>
2022-01-25 16:00:20 +0100

    oj

---
+++ app/src/main/java/fr/isen/PEDEPRAT/androidrestaurant/HomeActivity.kt
@@ -0,0 +1,29 @@
+package fr.isen.PEDEPRAT.androidrestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.R
+import android.app.Activity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import fr.isen.PEDEPRAT.androidrestaurant.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonEntrees.setOnClickListener{
+            Toast.makeText(self,"entree",Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
+
